b1dbc3552a3824b5839dc7a4a80be6a6b22cd71f
birdalugureren <birdalugureren@gmail.com>
2020-11-25 21:49:08 +0300

    Grading Students

---
+++ solution_10.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+func gradingStudents(grades []int32) []int32 {
+    // Write your code here
+    var finalGrade []int32
+    
+    for _, grade := range grades{
+        if grade < 38{
+            finalGrade = append(finalGrade,grade)
+        }else{
+            
+            remainder := 5 - grade % 5
+            if remainder < 3 {
+                grade = grade + remainder
+            }
+            finalGrade = append(finalGrade, grade)
+        }
+    }
+    return finalGrade
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    gradesCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+
+    var grades []int32
+
+    for i := 0; i < int(gradesCount); i++ {
+        gradesItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+        checkError(err)
+        gradesItem := int32(gradesItemTemp)
+        grades = append(grades, gradesItem)
+    }
+
+    result := gradingStudents(grades)
+
+    for i, resultItem := range result {
+        fmt.Fprintf(writer, "%d", resultItem)
+
+        if i != len(result) - 1 {
+            fmt.Fprintf(writer, "\n")
+        }
+    }
+
+    fmt.Fprintf(writer, "\n")
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
